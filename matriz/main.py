6250694619361b1d64f1b6586242077b8f72f533
adriannborella@hotmail.com <adriannborella@hotmail.com>
2021-01-23 00:52:18 -0300

    [FIX] Remove line

--- matriz/main.py
+++ matriz/main.py
@@ -69,5 +69,4 @@ def consecutive(data, stepsize=1):
     return False
 
 if __name__ == '__main__':    
-    search([[4, 0, 1, 2, 3],[4, 1, 3, 4, 3],[2, 4, 3, 0, 4],[1, 2, 3, 3, 4],[1, 4, 3, 1, 3]])
     doctest.testmod()
